bb97a57fcfcbff585f1fc8ec14db4e7ee68e7ac5
Lenoctambule <106790775+lenoctambule@users.noreply.github.com>
2026-03-11 22:30:03 +0100

    fix: bypass google oauth2 scheme policy

--- mobile/src/screens/RegisterScreen.tsx
+++ mobile/src/screens/RegisterScreen.tsx
@@ -11,7 +11,6 @@ import {
 } from 'react-native';
 import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
-import { makeRedirectUri } from 'expo-auth-session';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/AppNavigator';
 import { useAuthStore } from '../store/authStore';
@@ -70,13 +69,9 @@ export default function RegisterScreen({ navigation }: Props) {
     }
   };
 
-  const redirectUri = makeRedirectUri({ scheme: 'musicroom', path: 'auth' });
-
   const [googleRequest, googleResponse, promptGoogleAsync] = Google.useIdTokenAuthRequest({
     clientId: process.env.EXPO_PUBLIC_GOOGLE_CLIENT_ID,
-    iosClientId: process.env.EXPO_PUBLIC_GOOGLE_IOS_CLIENT_ID,
-    androidClientId: process.env.EXPO_PUBLIC_GOOGLE_ANDROID_CLIENT_ID,
-    redirectUri,
+    redirectUri: "https://auth.expo.io/@bilelghandri/musicroom",
   });
 
   useEffect(() => {
